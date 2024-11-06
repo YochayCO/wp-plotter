38091eef9c6f7f46e15022ac4353ad9e5452910c
Yochay Cohen-Or <yochai.cohenor@gmail.com>
2024-11-06 11:30:47 +0200

    Change to iframe

--- src/render.php
+++ src/render.php
@@ -10,96 +10,20 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 	
-	// Stubs for state variables that will be determined in editor
-	$surveyOptions = array( array( 'id' => '2022', 'name' => '2022 Survey' ) );
-	
 	// Add the global state.
-	wp_interactivity_state( 'plotter-ts', array( 
-		'urlBase' => '/', 
-		'surveyOptions' => $surveyOptions,
-	) );
-
-	// Initialize context
-	$context = array(
-		'xValue' => '',
-		'yValue' => '',
-		'surveyId' => '',
-		'survey' => null,
-	);
+	wp_interactivity_state( 'plotter-ts', array( 'height' => '600' ) );
 ?>
 
 <div
 	<?php echo get_block_wrapper_attributes() ?>
 	data-wp-interactive="plotter-ts"
-	<?php echo wp_interactivity_data_wp_context($context) ?>
-	data-wp-watch="actions.updateGraph"
 >
-
-	<div class="select-container">
-        <label for="select-x">Select survey</label>
-        <select id="select-survey" name="select_value" data-wp-on--change="actions.handleSelectSurvey">
-            <option value="">Select survey</option>
-			<template data-wp-each="state.surveyOptions">
-				<option data-wp-bind--value="context.item.id" data-wp-text="context.item.name"></option>
-			</template>
-        </select>
-    </div>
-	
-	<div class="default-invisible" data-wp-class--visible="extraContext.isSurveyLoaded">
-		<div class="select-container">
-			<label for="select-x">Select X</label>
-			<select id="select-x" name="select_value" data-wp-bind--value="context.xValue" data-wp-on--change="actions.handleSelectX">
-				<option value="">Select X</option>
-				<template data-wp-each="extraContext.questionItems">
-					<option data-wp-bind--value="context.item.column" data-wp-text="context.item.description"></option>
-				</template>
-			</select>
-		</div>
-	
-		<div class="select-container">
-			<label for="select-y">Select Y</label>
-			<select id="select-y" name="select_value" data-wp-bind--value="context.yValue" data-wp-on--change="actions.handleSelectY">
-				<option value="">Select Y</option>
-				<template data-wp-each="extraContext.quantityQuestionItems">
-					<option data-wp-bind--value="context.item.column" data-wp-text="context.item.description"></option>
-				</template>
-			</select>
-		</div>
-	
-		<!-- TODO: create real interactive graph -->
-		<div class="default-invisible" data-wp-class--visible="extraContext.isGraphVisible">
-			<div id="plot-container">
-				<svg width='100%' height=400px>
-					<g transform="translate(50, 30)">
-						<g data-wp-bind--transform="componentProps.translation">
-							<line
-								data-wp-bind--x1="componentProps.xScale.halfWidth"
-								data-wp-bind--x2="componentProps.xScale.halfWidth"
-								data-wp-bind--y1="componentProps.yScale.min"
-								data-wp-bind--y2="componentProps.yScale.max"
-								stroke="black"
-								width=40
-							/>
-							<rect
-								x="0"
-								data-wp-bind--y="componentProps.yScale.q3"
-								data-wp-bind--width="componentProps.xScale.width"
-								data-wp-bind--height="componentProps.yScale.boxHeight"
-								stroke="black"
-								fill="#ead4f5"
-							/>
-							<line
-								x1="0"
-								data-wp-bind--x2="componentProps.xScale.width"
-								data-wp-bind--y1="componentProps.yScale.median"
-								data-wp-bind--y2="componentProps.yScale.median"
-								stroke="black"
-								width=40
-							/>
-						</g>
-					</g>
-				</svg>
-			</div>
-		</div>
-	</div>
+	<iframe
+		id="plot-container" 
+		src="http://127.0.0.1:5173/"
+		width="100%" 
+		data-wp-bind--height="state.height"
+		allow="fullscreen" 
+	></iframe>
 </div>
+
--- src/view.ts
+++ src/view.ts
@@ -1,48 +1,14 @@
-import * as d3 from 'd3';
 import { store } from '@wordpress/interactivity';
 import actions from './utils/actions';
 import extraContext from './utils/extraContext';
 import { ServerState } from './types/state';
 
-// TODO: remove graph POC and add real functionality
-const xScaleKeys = ['0'];
-const yScale = d3
-	.scaleLinear()
-	.domain([0, 10])
-	.range([380, 0]);
-const xScale = d3
-	.scaleBand()
-	.domain(xScaleKeys)
-	.range([0, 580])
-	.padding(0.25);
-
 const storeDef = store( 'plotter-ts', {
-	componentProps: {
-		boxes: [{
-			group: '0',
-			min: 2,
-			q1: 3,
-			median: 5,
-			q3: 6,
-			max: 9,
-		}],
-		xScale: {
-			width: xScale.bandwidth(),
-			halfWidth: xScale.bandwidth() / 2,
-			group: xScale('0'),
-		},
-		yScale: {
-			min: yScale(2),
-			q1: yScale(3),
-			median: yScale(5),
-			q3: yScale(6),
-			max: yScale(9),
-			boxHeight: yScale(3) - yScale(6)
-		},
-		translation: `translate(${xScale('0')},0)`
+	state: {
+		height: window.innerHeight * 0.9,
 	},
 	actions,
-	extraContext
+	extraContext,
 });
 
 type Store = ServerState & typeof storeDef;
